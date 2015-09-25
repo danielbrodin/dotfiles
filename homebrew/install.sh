d9638ea970710750a9345852903d1217217942a7
Daniel Brodin <hej@danielbrodin.se>
2015-09-25 10:20:47 +0200

    Add own preferences

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -18,10 +18,31 @@ then
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
   fi
-  
+
 fi
 
 # Install homebrew packages
 brew install grc coreutils spark
+
+# Install Cask
+brew install caskroom/cask/brew-cask
+
+# Install native apps
+brew cask install the-unarchiver
+brew cask install bettertouchtool
+brew cask install dropbox
+
+brew cask install atom
+brew cask install sequel-pro
+brew cask install kitematic
+# brew cask install sourcetree
+brew cask install github-desktop
+
+brew cask install google-chrome
+brew cask install firefox
+
+# Fonts
+brew tap caskroom/fonts
+brew cask install font-ubuntu
 
 exit 0
