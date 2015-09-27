93f6ef10c40c28d0852ed13ce215712590f9a0c5
Daniel Brodin <hej@danielbrodin.se>
2015-09-27 18:27:41 +0200

    Add some more apps

--- homebrew/cask.sh
+++ homebrew/cask.sh
@@ -10,18 +10,23 @@ brew install caskroom/cask/brew-cask
 
 # Install native apps
 echo " Installing applications."
+# Good to have
 brew cask install the-unarchiver
 brew cask install bettertouchtool
 brew cask install dropbox
 
+# Dev
 brew cask install atom
 brew cask install sequel-pro
 brew cask install kitematic
-# brew cask install sourcetree
+brew cask install sourcetree
 brew cask install github-desktop
-
 brew cask install google-chrome
 brew cask install firefox
+
+# Fun stuff
+brew cask install battle-net
+brew cask install spotify
 
 # Install Fonts
 echo " Installing fonts."
